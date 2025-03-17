f4ae46f579a4b0c75396169b084e1fdbe17e37bb
Elelva02 <santiagobeltrans@usantotomas.edu.co>
2025-03-17 13:38:28 -0500

    Punto1.py

--- Punto1.py
+++ Punto1.py
@@ -1,26 +1,10 @@
 import streamlit as st
 import polars as pl  
-import plotly.express as px
 
-st.title("Análisis de Operaciones y Calificación de Usuarios")
+st.title("Verificación de Tipos en Polars")
 
 # Cargar datos con Polars
 df = pl.read_excel('depositos_oinks.xlsx')
 
-# Verificar los tipos de columnas
-st.write("Tipos de columnas:", df.schema)
-
-# Verificar y convertir operation_date a Date si es necesario
-if df.schema["operation_date"] == pl.Utf8:
-    df = df.with_columns(pl.col("operation_date").str.to_date("%Y-%m-%d"))
-elif df.schema["operation_date"] == pl.Int64:
-    df = df.with_columns(pl.col("operation_date").cast(pl.Date))
-elif df.schema["operation_date"] == pl.Float64:
-    df = df.with_columns(pl.col("operation_date").cast(pl.Date))
-else:
-    st.error("El tipo de 'operation_date' no es compatible.")
-    st.stop()
-
-# Mostrar tabla original
-st.subheader("Vista previa de los datos originales")
-st.dataframe(df.head(50))
+# Mostrar esquema de columnas
+st.write("Esquema del DataFrame:", df.schema)
